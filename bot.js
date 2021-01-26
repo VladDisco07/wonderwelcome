be30bc8a0afc5cc6e877cfd0ae921cbc1ac463ba
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2021-01-26 21:42:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,17 @@
-/*
-  A bot that welcomes new guild members when they join
-*/
-
-// Import the discord.js module
 const Discord = require('discord.js');
 
-// Create an instance of a Discord client
 const client = new Discord.Client();
 
-// The token of your bot - https://discordapp.com/developers/applications/me
 const token = 'NDA5MDMyODQxNDg2NDAxNTM2.DVYzgQ.ruwhnY2CFa7X8Oeixaq1tVo1gQo';
 
-// The ready event is vital, it means that your bot will only start reacting to information
-// from Discord _after_ ready is emitted
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-  // Send the message to a designated channel on a server:
   const channel = member.guild.channels.find('name', 'discutii-generale');
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
+  /if (!channel) return;
   channel.send(`[+] ${member}`);
 });
 
-// Log our bot in
 client.login(token);
